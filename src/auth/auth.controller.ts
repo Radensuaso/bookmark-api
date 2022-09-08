21244e0f18e035ea431ade17fcdc93335cd391c8
André Sousa <andrels9283@gmail.com>
2022-09-08 16:19:07 +0100

    signUp and signIn under way

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private auth: AuthService) {}
 
   @Post('signUp')
-  signUp() {
-    return this.authService.singUp();
+  signUp(@Body() dto: AuthDto) {
+    return this.auth.singUp(dto);
   }
 
   @Post('signIn')
-  signIn() {
-    return this.authService.singIn();
+  signIn(@Body() dto: AuthDto) {
+    return this.auth.singIn(dto);
   }
 }
